43921e1e4af4e954b511cf699d0682ae9333b386
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2019-12-27 12:53:32 -0300

    update main class name

--- src/main/java/com/br/pantheon/heimdall/Heimdall.java
+++ src/main/java/com/br/pantheon/heimdall/Heimdall.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeimdallApplication {
+public class Heimdall {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HeimdallApplication.class, args);
+		SpringApplication.run(Heimdall.class, args);
 	}
 
 }
