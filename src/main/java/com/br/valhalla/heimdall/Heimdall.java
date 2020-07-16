0213f99fda39cd61e442a87f45873414eb9081ed
Eder Celeste Nunes Júnior <edercnj@gmail.com>
2020-07-16 09:04:10 -0300

    add config files

--- src/main/java/com/br/valhalla/heimdall/Heimdall.java
+++ src/main/java/com/br/valhalla/heimdall/Heimdall.java
@@ -1,4 +1,4 @@
-package com.br.pantheon.heimdall;
+package com.br.valhalla.heimdall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
